4cc8a38d4aea6f7d930263dd95bb21f171a842b6
magikfinance <97064767+magikfinance@users.noreply.github.com>
2022-09-09 16:58:42 +0100

    update aprs

--- src/magik-finance/MagikFinance.ts
+++ src/magik-finance/MagikFinance.ts
@@ -437,19 +437,19 @@ export class MagikFinance {
     } if (depositTokenName.startsWith('MSHARE-FTM-LP')) {
       return rewardPerSecond.mul(595).div(59500);
     } if (depositTokenName.startsWith('MAGIK-FTM-LP')) {
-      return rewardPerSecond.mul(5950).div(59500);
+      return rewardPerSecond.mul(8330).div(59500);
     } if (depositTokenName.startsWith('MAGIK')) {
-      return rewardPerSecond.mul(100).div(59500);
+      return rewardPerSecond.mul(0).div(59500);
     } if (depositTokenName ===('MS-MAGIK-FTM')) {
       return rewardPerSecond.mul(10710).div(59500);
     } if (depositTokenName.startsWith('MS-MAGIK-MSHARE')) {
-      return rewardPerSecond.mul(1785).div(59500);
+      return rewardPerSecond.mul(0).div(59500);
     } if (depositTokenName.startsWith('MSHARE-FTM-MS')) {
-      return rewardPerSecond.mul(14775).div(59500);
+      return rewardPerSecond.mul(26675).div(59500);
     } if (depositTokenName.startsWith('MS-MAGIK-MIM')) {
-      return rewardPerSecond.mul(10710).div(59500);
+      return rewardPerSecond.mul(1190).div(59500);
     } if (depositTokenName.startsWith('MSHARE-MIM-MS')) {
-      return rewardPerSecond.mul(14875).div(59500);
+      return rewardPerSecond.mul(5950).div(59500);
     }
     
     
